0ff64b19ce32f803309f43aa03749a0872e7e027
ye546 <anton_bosse@hotmail.com>
2019-05-01 23:26:34 +0200

    skiten fungerar faktiskt nu lel

--- php/galleri.php
+++ php/galleri.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,76 +6,45 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/bootstrap-4.3.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap-4.3.1-dist/css/bootstrap-grid.css">
     <title>Galleri</title>
 </head>
 <body>
     <div class="container">
+    
         <div class="jumbotron">
+        <a href="../index.html">☇ Hem</a>
+        <h1>Bildgalleri.</h1>
             <div class="row">
                 <?php
-                    /*DENNA KOD BEHÖVER REWORKING, FÖR DEN FUNGERAR INTE AV NÅGON ANLEDNING*/
-
                     //skanna alla kataloger som finns 
-                    $dir = scandir("../imgs");
-                    $handle = null;
-                    $n = null;
+                        //set main directory
+                        $mainDir = '../imgs';
 
-                    foreach($dir as $d)
-                    {
-                        //kolla om katalogen finns
-                        if(is_dir($d))
-                        {
-                            //kolla om vi kan öppna katalogen
-                            if($handle = opendir($d)) //checking if return value is != FALSE
+                        //gets sub directories of PDFS directory
+                        $subDirectories = scandir($mainDir);
+
+                        //removes the first two indexes in the directories array that are just dots
+                        unset($subDirectories[0]);
+                        unset($subDirectories[1]);
+
+                        // first loop through all sub directories ...
+                        foreach($subDirectories as $subDirectory) {
+
+                            //echo '<h1>'.$subDirectory.'</h1>';
+                            // ... and then loop through all files in each sub directory
+                            foreach(glob($mainDir.'/'.$subDirectory.'/*') as $file) 
                             {
-                                $scanDir = readdir($handle);
-                                foreach($scanDir as $f)
+                                $file_extension = pathinfo($file);
+                                if($file_extension['extension'] == "png" || $file_extension['extension'] == "jpg")
                                 {
-                                     //hämta filändelse
-                                    $p_info = pathinfo($f);
-                                    //visa endast bilder om det är png eller jpg fil
-                                    $path = $d."/".$f;
-                                    if($p_info['extension'] == "png" || $p_info['extension'] == "jpg" )
-                                        echo "<img class='rounded' height='100px' width='100px'src='$path' alt='bild'>";
+                                    echo "<a href='$file'><img class='rounded' height='100px' width='100px'src='$file' alt='bild'></a>";
                                 }
                             }
                         }
-                        else if(!is_dir($d))
-                        {
-                            echo "<h1>not a dir</h1>";
-                        }
-                        
-                    }
-                    
-
-                    /*
-                    $cats = scandir("../imgs/cats");
-                    $dogs = scandir("../imgs/dogs");
-
-                    foreach($cats as $c)
-                    {
-                        //hämta filändelse
-                        $p_info = pathinfo($c);
-                        //visa endast bilder om det är png eller jpg fil
-                        if($p_info['extension'] == "png" || $p_info['extension'] == "jpg" )
-                            echo "<a href='../imgs/cats/$c'><img class='rounded' height='100px' width='100px'src='../imgs/cats/$c' alt='bild'></a>";
-                    }
-
-                    foreach($dogs as $d)
-                    {
-                        //hämta filändelse
-                        $p_info = pathinfo($d);
-                        //visa endast bilder om det är png eller jpg fil
-                        if($p_info['extension'] == "png" || $p_info['extension'] == "jpg" )
-                            echo "<a href='../imgs/dogs/$d'><img class='rounded' height='100px' width='100px'src='../imgs/dogs/$d' alt='bild'></a>";
-                    }
-                    */
                 ?>
             </div>
         </div>
     </div>
-
-
-
 </body>
 </html>
