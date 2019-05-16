b706de74f347dd784739db860c1c2564d5f5dd22
ye546 <anton_bosse@hotmail.com>
2019-05-16 21:06:47 +0200

    katalognamn
    katalognamnet syns nu som titel och i dokumentet ovanför bilderna

--- php/index.php
+++ php/index.php
@@ -11,30 +11,34 @@
 <div class="container">
     <div class="jumbotron">
     <a href="../../php/categories.php">☇ Tillbaka</a>
+            <?php
+                //katalognamn som titel                
+                $dirname = basename(__DIR__);
+                echo "<title>$dirname</title>";
+                echo "<h1>Här visas bilder från <span style='font-weight: bold;'>$dirname</span></h1>";
+            ?>
+
             <div class="row">
-                <?php
-                    //skanna katalogen till en fält
-                    $dir = scandir(".");
+            <?php
+                //skanna katalogen till en fält
+                $dir = scandir(".");
                     
-                    //tar bort dem två första indexeringarna ur fältet för dem två är bara '.' & '..'
-                    unset($dir[0]);
-                    unset($dir[1]);
+                //tar bort dem två första indexeringarna ur fältet för dem två är bara '.' & '..'
+                unset($dir[0]);
+                unset($dir[1]);
 
-                    //katalognamn som titel
-                    $dirname = basename(__DIR__);
-                    echo "<title>$dirname</title>";
-
-                    foreach($dir as $d)
-                    {
-                        $file_extension = pathinfo($d);
+                foreach($dir as $d)
+                {
+                    $file_extension = pathinfo($d);
                         
-                        //än så länge hanterar vi bara png och jpg filer, skippa att visa allting annat.
-                        if($file_extension['extension'] == "png" || $file_extension['extension'] == "jpg")
-                        {
-                            echo "<a href='$d'><img class='rounded' height='100px' width='100px'src='$d' alt='bild'></a>";
-                        }
+                    //än så länge hanterar vi bara png och jpg filer, skippa att visa allting annat.
+                    if($file_extension['extension'] == "png" || $file_extension['extension'] == "jpg")
+                    {
+                        echo "<a href='$d'><img class='rounded' height='100px' width='100px'src='$d' alt='bild'></a>";
                     }
-                ?>
+                }
+                    
+            ?>
             </div>
         </div>
     </div>
